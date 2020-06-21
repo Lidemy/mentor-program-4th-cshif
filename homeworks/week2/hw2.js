3e5ced34a29bd80abc9cad3230be9ffed6d9adcd
cshif <cooshif@gmail.com>
2020-06-21 13:47:21 +0800

    finished Week 2 hw3.js (without ESLint)

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,32 @@
+// hw2：首字母大寫
+// 給定一字串，把第一個字轉成大寫之後「回傳」，若第一個字不是英文字母則忽略。
+
+var str = '';
+
 function capitalize(str) {
-  
+	str = str.split('');
+	// console.log(str);
+	if (str[0].charCodeAt(0) >= 97 && str[0].charCodeAt(0) <= 122) {
+		strUC = str[0].toUpperCase(); // H
+		str.shift(); // remove + start
+		str.unshift(strUC); // add + start
+		// console.log(str);
+	} else {
+		// console.log(str);
+		// return str;
+	}
+	console.log(str.join(''));
+	return str.join('');
 }
 
-console.log(capitalize('hello'));
+/*
+- function: 接收字串，並把字串切成陣列
+- for loop:
+- if: 檢查陣列 index[0] 是不是英文字母大寫，是的話，忽略不動；不是，轉成大寫後回傳；不是英文字母，忽略
+*/
+
+capitalize('hello');
+capitalize('Gorgeous!');
+capitalize('amazing!');
+capitalize(':D');
+capitalize('prototype.shift()');
--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,37 @@
+// hw3：反轉字串
+// 給定一個字串，請「印出」反轉之後的樣子（不能使用內建的 reverse 函式）
+
+const str = '';
+
+// my thoughts:
+// 把最後一個字母 .push() 到新陣列 strSplitNew，重複 (str.length-1) 遍
+
+/*
+updates: 
+0. 分割陣列 strSplit 和新陣列 strArr 分開操作
+1. 把原始字串 str 的第 i 個位置的字母推到新陣列 strArr，重複 (str.length-1) 遍
+2. 去掉分割陣列 strSplit 的第 (str.length-1-i) 位
+3. 重複 2. 和 3. (str.length-1) 遍
+4. 合併陣列變字串，輸出
+*/
+
 function reverse(str) {
-  
+	let strSplit = str.split(''); // strSplit 是一個陣列
+	let strArr = [];
+	for(let i = 0; i <= str.length-1; i++){
+		strArr.push(str[str.length-1-i]); // .push() add element(s) to the end of array and return the new length
+		strSplit.pop(); // .pop() remove the last and return it
+		// console.log(strArr);
+	}
+	let strOutput = strArr.join(''); // .join() return a string
+	console.log(strOutput);
+	return strOutput;
 }
 
-reverse('hello');
+// function: 接收字串，拆成陣列
+// for loop: 把第一個字母調到第一位，重複 str.length 遍
+
+reverse('hello'); // olleh
+reverse('function'); // noitcnuf
+reverse('先別急著寫 leetcode');
+
