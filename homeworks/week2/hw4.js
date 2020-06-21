d6207eba726bc62ea193f7980cb9e32a2438bc1f
cshif <cooshif@gmail.com>
2020-06-21 16:20:22 +0800

    finished Week 2 hw4.js & hw4_advanced.js (without ESLint)

--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,13 +1,25 @@
 // hw4：印出因數
 // 先幫大家複習一下數學，給定一個數字 n，因數就是所有小於等於 n 又可以被 n 整除的數，所以最明顯的例子就是 1 跟 n，這兩個數一定是 n 的因數。現在請寫出一個函式來「印出」所有的因數
 
-const n;
+debugger
+
+var n;
 
 // my thoughts:
-// 
+// n 除以 1~n 之間的每個數字，印出餘數為零的數字
 
 function printFactor(n) {
-  
+	for(let i = 1; i <= n; i++){
+		if (n%i === 0) {
+			console.log(i);
+		}
+	}
+	return;
 }
 
-printFactor(10);
+// function: 接收數字
+// for loop: 跑 1~n
+// if: 若餘數為零，回傳
+
+printFactor(41);
+printFactor(12);
---
+++ homeworks/week2/hw4_advanced.js
@@ -0,0 +1,38 @@
+// 印出非質數數字 n 的因數；若 n 是質數，印出「這個數字是質數」
+// 挑戰把因數印在同一列
+
+debugger
+
+var num;
+
+// my thoughts:
+// n 除以 1~n 之間的每個數字，把可以整除 n 的數字放一起
+// 如果可以整除 n 的數字只有 n 自己和 1，顯示「這個數字是質數」
+// 如果可以整除 n 的數字不只有 n 和 1，顯示「這個數字的因數有：1, 2, 3, 4, 6, 12」
+
+function judgeFactor(num) {
+	let factorArr = [];
+	for(let i = 1; i <= num; i++){
+		if (num%i === 0) {
+			factorArr.push(i);
+		} // num 在跑完 for loop 後就消失惹
+	}
+	//console.log(factorArr);
+	return printFactor(factorArr, num);
+}
+
+function printFactor(factorArr, num) {
+	if (factorArr[0] === 1 && factorArr[1] === num) {
+		console.log(num + ' 是質數。');
+	} else {
+		console.log(num + ' 的因數有：' + factorArr.join(', '));
+	}
+	return;
+}
+
+judgeFactor(3);
+judgeFactor(7);
+judgeFactor(41);
+judgeFactor(12);
+judgeFactor(87);
+judgeFactor(160);
